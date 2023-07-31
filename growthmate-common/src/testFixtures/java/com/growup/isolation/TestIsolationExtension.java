8c8a9e11b883eb0d75e0b33bb165da3e4d012e52
does <ldk980130@gmail.com>
2023-07-31 23:29:47 +0900

    style: 메서드 이름 변경

--- growthmate-common/src/testFixtures/java/com/growup/isolation/TestIsolationExtension.java
+++ growthmate-common/src/testFixtures/java/com/growup/isolation/TestIsolationExtension.java
@@ -8,11 +8,11 @@ class TestIsolationExtension implements AfterEachCallback {
 
     @Override
     public void afterEach(ExtensionContext context) {
-        DatabaseCleaner databaseCleaner = getWannaflyDatabaseManager(context);
+        DatabaseCleaner databaseCleaner = getDatabaseManager(context);
         databaseCleaner.truncateTables();
     }
 
-    private DatabaseCleaner getWannaflyDatabaseManager(ExtensionContext context) {
+    private DatabaseCleaner getDatabaseManager(ExtensionContext context) {
         return (DatabaseCleaner) SpringExtension
                 .getApplicationContext(context)
                 .getBean("databaseCleaner");
