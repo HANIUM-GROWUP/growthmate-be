3c350db28b39c3583abca51dcba424eec21787ca
does <ldk980130@gmail.com>
2023-08-01 23:40:56 +0900

    feat: Member 도메인 엔티티 생성

---
+++ growthmate-core/src/main/java/com/growup/growthmate/member/domain/Member.java
@@ -0,0 +1,17 @@
+package com.growup.growthmate.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+}
---
+++ growthmate-core/src/main/resources/db/migration/V1__create-member.sql
@@ -0,0 +1,5 @@
+CREATE TABLE member
+(
+    member_id BIGINT AUTO_INCREMENT NOT NULL,
+    CONSTRAINT pk_member PRIMARY KEY (member_id)
+);
