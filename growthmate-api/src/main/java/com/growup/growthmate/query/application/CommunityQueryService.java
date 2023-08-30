72a2109b2d0a799a568e099ca0c699628e243192
does <ldk980130@gmail.com>
2023-08-30 20:20:37 +0900

    refactor: mapper로 dto 변환 로직 리팩터링

--- growthmate-api/src/main/java/com/growup/growthmate/query/application/CommunityQueryService.java
+++ growthmate-api/src/main/java/com/growup/growthmate/query/application/CommunityQueryService.java
@@ -2,16 +2,14 @@ package com.growup.growthmate.query.application;
 
 import com.growup.growthmate.BusinessException;
 import com.growup.growthmate.community.post.exception.PostException;
-import com.growup.growthmate.query.dto.*;
+import com.growup.growthmate.query.dto.CommunityResponseMapper;
+import com.growup.growthmate.query.dto.PagingParams;
 import com.growup.growthmate.query.dto.request.CommentQueryRequest;
 import com.growup.growthmate.query.dto.request.PostDetailRequest;
 import com.growup.growthmate.query.dto.response.CommentResponse;
 import com.growup.growthmate.query.dto.response.PostDetailResponse;
 import com.growup.growthmate.query.dto.response.PostPreviewResponse;
-import com.growup.growthmate.query.repository.projection.CommentProjection;
-import com.growup.growthmate.query.repository.projection.PostDetailProjection;
 import com.growup.growthmate.query.repository.CommunityQueryRepository;
-import com.growup.growthmate.query.repository.projection.PostPreviewProjection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,21 +22,12 @@ import java.util.List;
 public class CommunityQueryService {
 
     private final CommunityQueryRepository communityQueryRepository;
+    private final CommunityResponseMapper mapper;
 
     public PostDetailResponse findPostDetail(PostDetailRequest request) {
         return communityQueryRepository.findPostDetail(request.postId())
-                .map(post -> toPostDetailResponse(post, request.loginId()))
+                .map(post -> mapper.toResponse(post, isMine(post.getMember_id(), request.loginId())))
                 .orElseThrow(this::throwNotFoundException);
-    }
-
-    private PostDetailResponse toPostDetailResponse(PostDetailProjection projection, Long loginId) {
-        return new PostDetailResponse(
-                projection.getName(),
-                projection.getTitle(),
-                projection.getContent(),
-                projection.getCreated_at(),
-                isMine(projection.getMember_id(), loginId)
-        );
     }
 
     private BusinessException throwNotFoundException() {
@@ -48,34 +37,14 @@ public class CommunityQueryService {
 
     public List<PostPreviewResponse> findPostPreviews(Long companyId, PagingParams params) {
         return communityQueryRepository.findPostPreviews(companyId, params.getCursor(), params.getSize()).stream()
-                .map(this::getPostPreviewResponse)
+                .map(mapper::toResponse)
                 .toList();
-    }
-
-    private PostPreviewResponse getPostPreviewResponse(PostPreviewProjection projection) {
-        return new PostPreviewResponse(
-                projection.getPostId(),
-                projection.getName(),
-                projection.getTitle(),
-                projection.getContent().substring(0, 10),
-                projection.getCreatedAt(),
-                projection.getCommentCount()
-        );
     }
 
     public List<CommentResponse> findComments(CommentQueryRequest request, PagingParams params) {
         return communityQueryRepository.findComments(request.postId(), params.getCursor(), params.getSize()).stream()
-                .map(projection -> getCommentResponse(projection, request.loginId()))
+                .map(post -> mapper.toResponse(post, isMine(post.getMemberId(), request.loginId())))
                 .toList();
-    }
-
-    private CommentResponse getCommentResponse(CommentProjection projection, Long loginId) {
-        return new CommentResponse(
-                projection.getCommentId(),
-                projection.getName(),
-                projection.getContent(),
-                isMine(projection.getMemberId(), loginId)
-        );
     }
 
     private boolean isMine(Long memberId, Long loginId) {
