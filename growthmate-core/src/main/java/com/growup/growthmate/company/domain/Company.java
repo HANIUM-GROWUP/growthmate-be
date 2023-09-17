7adbe802f19786c2fa259fd839e1f5667606f92a
does <ldk980130@gmail.com>
2023-09-18 00:17:38 +0900

    feat: company 칼럼 수정

--- growthmate-core/src/main/java/com/growup/growthmate/company/domain/Company.java
+++ growthmate-core/src/main/java/com/growup/growthmate/company/domain/Company.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -17,4 +19,31 @@ public class Company {
 
     @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String ceo;
+
+    @Column(nullable = false)
+    private String scale;
+
+    @Column(nullable = false)
+    private String businessType;
+
+    @Column(nullable = false)
+    private String business;
+
+    @Column(nullable = false)
+    private LocalDateTime establishmentDate;
+
+    @Column(nullable = false)
+    private Long sales;
+
+    @Column(nullable = false)
+    private Long employeeNumber;
+
+    @Column(nullable = false)
+    private String address;
 }
