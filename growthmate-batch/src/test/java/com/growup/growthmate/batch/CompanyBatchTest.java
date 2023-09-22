f35afda16ab524281703a7b9ce9f2ad80637b4a3
does <ldk980130@gmail.com>
2023-09-22 22:04:03 +0900

    test: 회사 개수 검증 추가

--- growthmate-batch/src/test/java/com/growup/growthmate/batch/CompanyBatchTest.java
+++ growthmate-batch/src/test/java/com/growup/growthmate/batch/CompanyBatchTest.java
@@ -1,5 +1,7 @@
 package com.growup.growthmate.batch;
 
+import com.growup.growthmate.company.domain.Company;
+import com.growup.growthmate.fixture.CompanyFixtureRepository;
 import com.growup.growthmate.isolation.TestIsolation;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
@@ -9,7 +11,10 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @SpringBatchTest
@@ -19,12 +24,20 @@ class CompanyBatchTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private CompanyFixtureRepository companyFixtureRepository;
+
     @Test
     void 회사_배치를_실행한다() throws Exception {
         // when
         JobExecution actual = jobLauncherTestUtils.launchJob();
 
         // then
-        assertThat(actual.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+        List<Company> companies = companyFixtureRepository.findAll();
+        assertAll(
+                () -> assertThat(actual.getStatus()).isEqualTo(BatchStatus.COMPLETED),
+                () -> assertThat(companies).hasSize(29)
+        );
+
     }
 }
