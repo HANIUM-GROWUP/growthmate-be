884221d6f8eeebb5e58e1899142706a1233d560f
does <ldk980130@gmail.com>
2023-09-29 22:18:48 +0900

    fix: null 허용 sql 변경

--- growthmate-core/src/main/resources/db/migration/V3__company-update.sql
+++ growthmate-core/src/main/resources/db/migration/V3__company-update.sql
@@ -1,11 +1,11 @@
 ALTER TABLE company ADD CONSTRAINT comapny_name_unique UNIQUE (name);
 
 ALTER TABLE company MODIFY COLUMN image_url LONGTEXT null;
-ALTER TABLE company MODIFY COLUMN ceo null;
-ALTER TABLE company MODIFY COLUMN scale null;
-ALTER TABLE company MODIFY COLUMN business_type null;
-ALTER TABLE company MODIFY COLUMN business null;
-ALTER TABLE company MODIFY COLUMN establishment_date null;
-ALTER TABLE company MODIFY COLUMN sales null;
-ALTER TABLE company MODIFY COLUMN employee_number null;
-ALTER TABLE company MODIFY COLUMN address null;
+ALTER TABLE company MODIFY COLUMN ceo VARCHAR(255) null;
+ALTER TABLE company MODIFY COLUMN scale VARCHAR(255) null;
+ALTER TABLE company MODIFY COLUMN business_type VARCHAR(255) null;
+ALTER TABLE company MODIFY COLUMN business VARCHAR(255) null;
+ALTER TABLE company MODIFY COLUMN establishment_date datetime null;
+ALTER TABLE company MODIFY COLUMN sales BIGINT null;
+ALTER TABLE company MODIFY COLUMN employee_number BIGINT null;
+ALTER TABLE company MODIFY COLUMN address VARCHAR(255) null;
