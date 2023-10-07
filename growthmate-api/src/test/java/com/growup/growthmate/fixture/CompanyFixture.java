f689f3731d92f035bd3b38a8f16f515f41f9cab7
안정후 <austinan@naver.com>
2023-10-07 18:30:42 +0900

    fix: sql 상황

--- growthmate-api/src/test/java/com/growup/growthmate/fixture/CompanyFixture.java
+++ growthmate-api/src/test/java/com/growup/growthmate/fixture/CompanyFixture.java
@@ -15,10 +15,10 @@ public class CompanyFixture {
     public static final Long COMPANY_ID = 1L;
 
     // COMPANY DATA
-    public static final String NAME = "비트 망고";
+    public static final String NAME = "비트 망고1";
     public static final String IMAGE_URL = "picure.com";
     public static final String BUSINESSTYPE = "모바일 게임, 소프트웨어 공급업";
-    public static final LocalDateTime ESTABLISHMENTDATE = LocalDateTime.parse("1996-07-01 00:00:00", formatter);
+    public static final LocalDateTime ESTABLISHMENTDATE = LocalDateTime.parse("1985-07-01 00:00:00", formatter);
     public static final String ADDRESS = "경기도 성남시 분당구 대왕판교로645번길 14 네오위즈판교타워 3층 (우)13487";
     public static final BigInteger EMPLOYMENTNUMBER = BigInteger.valueOf(43);
     public static final BigInteger SALES = BigInteger.valueOf(Long.parseLong("139485530000"));
