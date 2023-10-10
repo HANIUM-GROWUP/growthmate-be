4ed27366c9069d0d350b45bde5c2cff6c1754e38
안정후 <austinan@naver.com>
2023-10-10 11:10:09 +0900

    fix: changing repository for using enum

--- growthmate-api/src/main/java/com/growup/growthmate/company/repository/find/CompanyFindRepositoryImpl.java
+++ growthmate-api/src/main/java/com/growup/growthmate/company/repository/find/CompanyFindRepositoryImpl.java
@@ -2,10 +2,8 @@ package com.growup.growthmate.company.repository.find;
 
 import com.growup.growthmate.company.domain.Company;
 import com.growup.growthmate.company.dto.find.CompanyDetailRequest;
-import com.growup.growthmate.company.dto.find.SortField;
 import com.growup.growthmate.company.dto.find.SortedCompanyRequest;
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.dsl.ComparableExpressionBase;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +11,6 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import static com.growup.growthmate.company.domain.QCompany.company;
-import static com.growup.growthmate.company.dto.find.SortField.ESTABLISHMENT_DATE;
-import static com.growup.growthmate.company.dto.find.SortField.SALES;
 
 @RequiredArgsConstructor
 public class CompanyFindRepositoryImpl implements CompanyFindRepository {
@@ -23,31 +19,10 @@ public class CompanyFindRepositoryImpl implements CompanyFindRepository {
 
     @Override
     public List<Company> findSortedCompanies(SortedCompanyRequest request) {
-
-        ComparableExpressionBase<?> orderByField;
-
-        SortField sortField;
-        try {
-            sortField = SortField.valueOf(request.sort().toUpperCase());
-        } catch (IllegalArgumentException e) {
-            sortField = SortField.ID; // 기본값 설정
-        }
-
-        switch (sortField) {
-            case ESTABLISHMENT_DATE:
-                orderByField = company.establishmentDate;
-                break;
-            case SALES:
-                orderByField = company.sales;
-                break;
-            default:
-                orderByField = company.id;
-                break;
-        }
-
+        CompanySortField companySortField = CompanySortField.from(request.sort());
         return jpaQueryFactory.selectFrom(company)
                 .where(getCursorCondition(request.cursor(), company.id))
-                .orderBy(orderByField.desc())
+                .orderBy(companySortField.getOrderExpression().desc())
                 .limit(request.size())
                 .fetch();
     }
