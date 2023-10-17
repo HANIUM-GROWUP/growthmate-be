86b48a0a990810eff639211ba61c44e1f3f13888
안정후 <austinan@naver.com>
2023-10-17 16:46:52 +0900

    feat: controller & service layer

--- growthmate-api/src/main/java/com/growup/growthmate/company/application/CompanyService.java
+++ growthmate-api/src/main/java/com/growup/growthmate/company/application/CompanyService.java
@@ -10,8 +10,10 @@ import com.growup.growthmate.company.dto.find.CompanyDetailRequest;
 import com.growup.growthmate.company.dto.find.CompanyDetailResponse;
 import com.growup.growthmate.company.dto.find.SortedCompanyRequest;
 import com.growup.growthmate.company.dto.find.SortedCompanyResponse;
+import com.growup.growthmate.company.dto.growth.CompanyGrowthResponse;
 import com.growup.growthmate.company.mapper.CompanyMapper;
 import com.growup.growthmate.company.repository.CompanyRepository;
+import com.growup.growthmate.company.repository.growth.projection.CompanyGrowthProjection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,5 +51,12 @@ public class CompanyService {
         return entityResponse
                 .map(companyMapper::toAnalysisDTO)
                 .orElseThrow(() -> new BusinessException(CompanyException.NO_FOUND_COMPANY.getHttpStatusCode(), CompanyException.NO_FOUND_COMPANY.getMessage()));
+    }
+
+    public List<CompanyGrowthResponse> findCompanyGrowth(Long companyId) {
+
+        List<CompanyGrowthProjection> entityResponse = companyRepository.findCompanyGrowth(companyId);
+
+        return companyMapper.toGrowthDTO(entityResponse);
     }
 }
