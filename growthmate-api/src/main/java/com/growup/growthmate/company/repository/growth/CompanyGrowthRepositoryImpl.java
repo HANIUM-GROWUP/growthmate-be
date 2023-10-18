91542db39cc7b43bb36a131e277d06b5ceea03c5
안정후 <austinan@naver.com>
2023-10-18 11:48:00 +0900

    fix: order by year

--- growthmate-api/src/main/java/com/growup/growthmate/company/repository/growth/CompanyGrowthRepositoryImpl.java
+++ growthmate-api/src/main/java/com/growup/growthmate/company/repository/growth/CompanyGrowthRepositoryImpl.java
@@ -18,6 +18,7 @@ public class CompanyGrowthRepositoryImpl implements CompanyGrowthRepository{
         return queryFactory.select(CompanyGrowthProjection.CONSTRUCTOR_EXPRESSION)
                 .from(companyGrowth)
                 .where(companyGrowth.companyId.eq(companyId))
+                .orderBy(companyGrowth.year.desc())
                 .fetch();
     }
 }
