ddb274616feeab67ea590b8dfec1bb3b9d0f5a57
does <ldk980130@gmail.com>
2023-10-26 23:26:33 +0900

    feat: company_news 테이블 및 CompanyNews 엔티티 추가

---
+++ growthmate-core/src/main/java/com/growup/growthmate/company/domain/Sentiment.java
@@ -0,0 +1,6 @@
+package com.growup.growthmate.company.domain;
+
+public enum Sentiment {
+
+    POSITIVE, NEGATIVE
+}
--- growthmate-core/src/main/resources/db/migration/V5__sentiment.sql
+++ growthmate-core/src/main/resources/db/migration/V5__sentiment.sql
@@ -2,10 +2,25 @@ CREATE TABLE company_sentiment
 (
     company_sentiment_id BIGINT AUTO_INCREMENT NOT NULL,
     company_id           BIGINT                NOT NULL,
-    positive_rate        DOUBLE                NULL,
-    negative_rate        DOUBLE                NULL,
+    positive_rate        DOUBLE                NOT NULL,
+    negative_rate        DOUBLE                NOT NULL,
     CONSTRAINT pk_companysentiment PRIMARY KEY (company_sentiment_id)
 );
 
 ALTER TABLE company_sentiment
     ADD CONSTRAINT FK_COMPANY_SENTIMENT_ON_COMPANY FOREIGN KEY (company_id) REFERENCES company (company_id);
+
+
+CREATE TABLE company_news
+(
+    company_news_id BIGINT AUTO_INCREMENT NOT NULL,
+    company_id      BIGINT                NOT NULL,
+    title           VARCHAR(255)          NOT NULL,
+    description   LONGTEXT              NOT NULL,
+    url             VARCHAR(255)          NOT NULL,
+    sentiment       VARCHAR(255)          NOT NULL,
+    CONSTRAINT pk_companynews PRIMARY KEY (company_news_id)
+);
+
+ALTER TABLE company_news
+    ADD CONSTRAINT FK_COMPANY_NEWS_ON_COMPANY FOREIGN KEY (company_id) REFERENCES company (company_id);
