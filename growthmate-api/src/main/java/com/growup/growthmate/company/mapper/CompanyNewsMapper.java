aaec5873e7787cd35394da849f9f9d53228df768
안정후 <austinan@naver.com>
2023-10-27 15:55:19 +0900

    feat: news & sentiment mapper setting

---
+++ growthmate-api/src/main/java/com/growup/growthmate/company/mapper/CompanyNewsMapper.java
@@ -0,0 +1,20 @@
+package com.growup.growthmate.company.mapper;
+
+import com.growup.growthmate.company.dto.news.CompanyNewsResponse;
+import com.growup.growthmate.company.repository.news.projection.CompanyNewsProjection;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyNewsMapper {
+
+    public CompanyNewsResponse toResponse(CompanyNewsProjection projection) {
+        return new CompanyNewsResponse(
+                projection.getCompanyId(),
+                projection.getTitle(),
+                projection.getDescription(),
+                projection.getUrl(),
+                projection.getSentiment()
+        );
+    }
+
+}
