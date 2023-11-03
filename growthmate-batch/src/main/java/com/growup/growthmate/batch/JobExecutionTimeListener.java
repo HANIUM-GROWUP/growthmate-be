26b60c9104bb2363066a7335f8823be07dc9f6e4
does <ldk980130@gmail.com>
2023-11-03 17:33:54 +0900

    feat: Step 성공 여부 로그 찍기

--- growthmate-batch/src/main/java/com/growup/growthmate/batch/JobExecutionTimeListener.java
+++ growthmate-batch/src/main/java/com/growup/growthmate/batch/JobExecutionTimeListener.java
@@ -1,10 +1,15 @@
 package com.growup.growthmate.batch;
 
 import com.growup.growthmate.support.log.ExecutionTimeLogger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.util.StopWatch;
 
+import java.util.Collection;
+
+@Slf4j
 public class JobExecutionTimeListener implements JobExecutionListener {
 
     private StopWatch stopWatch;
@@ -19,5 +24,15 @@ public class JobExecutionTimeListener implements JobExecutionListener {
     public void afterJob(JobExecution jobExecution) {
         stopWatch.stop();
         ExecutionTimeLogger.log("회사 Batch 전체", stopWatch.getTotalTimeMillis());
+        logStepStatuses(jobExecution);
+    }
+
+    private void logStepStatuses(JobExecution jobExecution) {
+        StringBuilder stepStatuses = new StringBuilder();
+        Collection<StepExecution> stepExecutions = jobExecution.getStepExecutions();
+        for (StepExecution stepExecution : stepExecutions) {
+            stepStatuses.append(stepExecution.getExitStatus()).append("\n");
+        }
+        log.info("각 Step 성공 여부 : \n" + stepStatuses);
     }
 }
